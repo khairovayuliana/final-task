e7900574771b77a5a194026932448afc09d61945
khairovayuliana <y.khayrova@inbox.ru>
2025-06-18 13:48:03 +0300

    Update and rename point.cpp to vampire.cpp

--- src/point.cpp
+++
@@ -1,9 +0,0 @@
-// #include <cmath>
-
-#include "point.h"
-
-// double rectangleSquare(Point a, Point b) {
-//     double width = std::abs(b.x - a.x);
-//     double height = std::abs(b.y - a.y);
-//     return width * height;
-// }
